8260f94297013c8a26b363638d8067f3f4abb561
reowicaksono <reo.wicaksono.2005356@gmail.com>
2025-02-13 11:17:14 +0700

    [update] tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  darkMode: 'class', 
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1a1a1d', 
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['dark'], 
+    },
+  },
+  plugins: [],
+};
