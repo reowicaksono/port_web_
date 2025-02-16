3026dbaecc13551891f5c9245b5c16f0714b439a
reowicaksono <reo.wicaksono.2005356@gmail.com>
2025-02-16 10:14:23 +0700

    [update] remove state in comment

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from './view/HomeView.jsx'
 import PortfolioView from './view/PortfolioView.jsx'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
       <Routes>
